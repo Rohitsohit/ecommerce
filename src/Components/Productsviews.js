fb165db95cf2319d308bd4d41067439e52125f42
Rohitsohit <itsrohitemail@gmail.com>
2023-12-11 20:36:37 -0500

    prduct details

--- src/Components/Productsviews.js
+++ src/Components/Productsviews.js
@@ -1,21 +1,49 @@
-import React from "react";
-import { useSelector } from "react-redux";
 
+import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
 import Productcard from "./ProductsView/Productcard";
 import Spinner from "./Spinner/Spinner";
 export default function Productview() {
   
-  const productsData = useSelector((state) => state.product);
+  var data = useSelector((state) => state.product);
+
+  const [productData, setproductData] = useState();
+  const handleCategory=(cat)=>{
+    const updatedList = data.filter((item) => item.productCategory === cat);
+    setproductData(updatedList);
+
+  }
+  useEffect(() => {
+    setproductData(data);
+  }, [])
   
-  return !productsData.length ? (
+  return !productData ? (
     <Spinner />
   ) : (
     <div className="row my-5 mx-5">
-      {productsData.map((product) => (
+      <div className="row">
+          <div className="col-12">
+            <h2 className="display-5 text-center">Latest Products</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="buttons text-center py-5">
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={()=>setproductData(data)} >All</button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={()=>{handleCategory("Phone")}} >Phone</button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={()=>{handleCategory("Hardware")}} >
+            Hardware
+          </button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={()=>{handleCategory("Laptop")}}>Laptop</button>
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={()=>{handleCategory("Accessories")}} >Computer Accessories</button>
+        </div>
+        
+    <div className="row my-5 mx-5">
+      {productData.map((product) => (
             
             <Productcard product={product}></Productcard>
     
       ))}
+    </div>
     </div>
   );
 }
